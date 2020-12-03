a884a5b891881aab68e0233ba712058abf62f1e6
Owen Richards <owen@biglemon.co.uk>
2020-12-03 10:50:37 +0000

    Day 3 challenge 2

--- owen/day3/index.test.ts
+++ owen/day3/index.test.ts
@@ -1,4 +1,4 @@
-import { countTrees } from './index';
+import { countTrees, multiplyArray } from './index';
 
 import input from './input.txt';
 
@@ -22,7 +22,27 @@ describe('day 3', () => {
   });
   test('challenge 1 real data', () => {
     const trees = countTrees(input, 31);
-    console.log(trees);
-    expect(trees).toBe(171);
+    expect(trees).toBe(178);
+  });
+  test('challenge 2 sample data', () => {
+    const trees = [
+      countTrees(sampleInput, 11, 1, 1),
+      countTrees(sampleInput, 11, 3, 1),
+      countTrees(sampleInput, 11, 5, 1),
+      countTrees(sampleInput, 11, 7, 1),
+      countTrees(sampleInput, 11, 1, 2),
+    ];
+    expect(trees).toEqual([2, 7, 3, 4, 2]);
+    expect(multiplyArray(trees)).toBe(336);
+  });
+  test('challenge 2 real data', () => {
+    const trees = [
+      countTrees(input, 31, 1, 1),
+      countTrees(input, 31, 3, 1),
+      countTrees(input, 31, 5, 1),
+      countTrees(input, 31, 7, 1),
+      countTrees(input, 31, 1, 2),
+    ];
+    expect(multiplyArray(trees)).toBe(3492520200);
   });
 });
--- owen/day3/index.ts
+++ owen/day3/index.ts
@@ -13,19 +13,44 @@ const prepare = (input: string): string[] => {
  * Cycle through each row and increase the index to capture the correct character
  * @param input String
  * @param maxRowIndex Total indexes in the row
+ * @param rightIncrement The number to jump right
+ * @param downIncrement The number to jump down
  */
-export const countTrees = (input: string, maxRowIndex: number) => {
+export const countTrees = (
+  input: string,
+  maxRowIndex: number,
+  rightIncrement: number = 3,
+  downIncrement: number = 1
+) => {
   const arr = prepare(input);
-  //   console.log(arr);
+
   let captured = [];
-  let index = 0;
-  arr.forEach((row: string) => {
-    if (index >= maxRowIndex) index -= maxRowIndex;
+  let rightIndex = 0;
+  let downIndex = 0;
+
+  while (downIndex <= arr.length - 1) {
+    if (rightIndex >= maxRowIndex) rightIndex -= maxRowIndex;
+
+    // Add the character to one big array
+    const row = arr[downIndex];
+    captured.push(row.charAt(rightIndex));
     // console.log(row, index, row.charAt(index));
-    captured.push(row.charAt(index));
-    index += 3;
-  });
+
+    // Increase the indexes to the next row and index position
+    rightIndex += rightIncrement;
+    downIndex += downIncrement;
+  }
+
   captured.shift(); // Remove the first one because it should start on the 2nd line
-  //   console.log(captured, captured.length);
   return captured.filter((e) => e === '#').length;
+};
+
+/**
+ * Multiply all items in an array
+ * @param arr Array of numbers to multiply
+ */
+export const multiplyArray = (arr: number[]) => {
+  return arr.reduce((total: number, no: number) => {
+    return total * no;
+  }, 1);
 };
